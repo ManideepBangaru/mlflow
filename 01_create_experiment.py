989abd341c52e0a83f0929885327794558e94683
manideepbangaru <bmd994@gmail.com>
2024-04-04 19:31:51 +0530

    completed experiment 1

--- 01_create_experiment.py
+++ 01_create_experiment.py
@@ -1,6 +1,9 @@
 import mlflow
 
 if __name__ == '__main__':
-    name = "testing_mlflow1"
-    artifact_location = "testing_mlflow1_artifacts"
-    tags = {"env":"dev","version":"1.0.0"}
+    # create a mlflow experiment
+    mlflow.create_experiment(
+            name = "testing_mlflow1",
+            artifact_location = "testing_mlflow1_artifacts",
+            tags = {"env":"dev","version":"1.0.0"},
+    )
