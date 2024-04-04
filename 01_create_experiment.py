a2cb8722450ed35ef23fb25ca771f17e696a28fd
manideepbangaru <bmd994@gmail.com>
2024-04-04 19:40:57 +0530

    created experiment 2

--- 01_create_experiment.py
+++ 01_create_experiment.py
@@ -1,9 +1,12 @@
 import mlflow
+from mlflow_utils import create_mlflow_experiment
 
 if __name__ == '__main__':
     # create a mlflow experiment
-    mlflow.create_experiment(
-            name = "testing_mlflow1",
-            artifact_location = "testing_mlflow1_artifacts",
-            tags = {"env":"dev","version":"1.0.0"},
-    )
+    # mlflow.create_experiment(
+    #         name = "testing_mlflow1",
+    #         artifact_location = "testing_mlflow1_artifacts",
+    #         tags = {"env":"dev","version":"1.0.0"},
+    # )
+    experiment_id = create_mlflow_experiment(experiment_name="testing_mlflow2", artifact_location="testing_mlflow2_artifacts", tags={"env":"dev","version":"1.0.0"})
+    print("Experiment ID : ", experiment_id)
