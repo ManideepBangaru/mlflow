c0fca429bd989a09b7042bc937bad443119d0a3b
ManideepBangaru <bmd994@gmail.com>
2024-11-16 00:29:00 +0530

    minor updates

--- 01_Using_MLFlow_Client_API.py
+++ 01_Using_MLFlow_Client_API.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from sklearn.ensemble import RandomForestRegressor
 
 # Configuring the MLflow Tracking Client
-client = MlflowClient(tracking_uri="http://127.0.0.1:8080")
+client = MlflowClient(tracking_uri="http://127.0.0.1:5000")
 
 # Searching experiments
 all_experiments = client.search_experiments()
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 A Project using MLflow
 
 ## To start server locally
-mlflow server --host 127.0.0.1 --port 8080
+mlflow ui # default on local 5000 port
+mlflow ui --host 127.0.0.1 --port 8080 # change host and port
